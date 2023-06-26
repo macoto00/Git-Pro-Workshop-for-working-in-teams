f8170f3b11d4cc1c66384e560d0c71156050a27e
DeenO37 <deen.bds@gmail.com>
2023-06-26 14:40:08 +0200

    greet modified

--- demo/src/Greet.java
+++ demo/src/Greet.java
@@ -1,6 +1,6 @@
 public class Greet {
     public static void greet(){
-        String greeting = "Ahoj";
+        String greeting = "Greetings";
         System.out.println(greeting);
         System.out.println("Tomáš");
     }
