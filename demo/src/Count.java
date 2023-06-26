0acb788f2b184a555f3bccf560aa2280391d4971
Slithercze <Slithercze@gmail.com>
2023-06-26 15:29:21 +0200

    b5

--- demo/src/Count.java
+++ demo/src/Count.java
@@ -1,5 +1,5 @@
 public class Count {
-    public static void count(int a){
-        System.out.println("You age is 21" + a + "1");
+    public static void count(int a, int b){
+        System.out.println("You age is 21" + a + "1" + b);
     }
 }
