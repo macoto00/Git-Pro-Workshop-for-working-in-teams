8d3aafb370e0ddcc9bf9de0b4fce1ac16b638941
DeenO37 <deen.bds@gmail.com>
2023-06-26 15:05:32 +0200

    lopar

--- demo/src/Cheer.java
+++ demo/src/Cheer.java
@@ -1,6 +1,6 @@
 public class Cheer {
     public void cheer(){
-        String name = "Khamil";
+        String name = "Lopar";
         System.out.println("Walk a dog dog");
         System.out.println(name);
         System.out.println("My chosen base language is Mira");
